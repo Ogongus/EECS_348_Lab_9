880e57ff80697fcf0dd3a75f64016d79da9b74a1
Ogongus <whjtan4357@gmail.com>
2025-10-28 20:19:21 -0500

    add bool readMatrices

--- main.cpp
+++ main.cpp
@@ -10,3 +10,27 @@ using namespace std;
 using Matrix = vector<vector<int>>;
 
 // Read matrices from file.
+bool readMatrices(const string& filename, int& N, Matrix& A, Matrix& B) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cout << "Error: Cannot open file" << filename << endl;
+        return false;
+    }
+
+    file >> N;
+    A.assign(N, vector<int>(N));
+    B.assign(N, vector<int>(N));
+
+    // Read matrix A
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            file >> A[i][j];
+
+    // Read matrix B
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            file >> B[i][j]
+
+    file.close();
+    return true;
+}
