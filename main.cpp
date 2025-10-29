e41b1a32d81a7e0bf4fc0b0f82e6c5beb7fd33e8
Ogongus <whjtan4357@gmail.com>
2025-10-28 20:15:53 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// main.cpp
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+using Matrix = vector<vector<int>>;
+
+// Read matrices from file.
